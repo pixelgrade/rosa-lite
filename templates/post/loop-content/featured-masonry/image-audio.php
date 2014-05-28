edab475ddbf93fb7e6d23457833bf5c6a0a494fb
Vlad Olaru <vlad.olaru@pixelgrade.com>
2014-05-28 10:08:08 +0300

    cleanup

--- templates/post/loop-content/featured-masonry/image-audio.php
+++
@@ -1,9 +0,0 @@
-<?php
-$audio_embed = get_post_meta($post->ID, wpgrade::prefix().'audio_embed', true)
-?>
-
-<?php if( ! empty($audio_embed)): ?>
-	<div class="article__featured-image">
-		<?php echo stripslashes(htmlspecialchars_decode($audio_embed)) ?>
-	</div>
-<?php endif; ?>
--- templates/post/loop-content/featured-masonry/image-video.php
+++
@@ -1,9 +0,0 @@
-<?php
-$video_embed = get_post_meta($post->ID, wpgrade::prefix().'video_embed', true);
-?>
-
-<?php if ( ! empty($video_embed)): ?>
-	<div class="article__featured-image">
-		<?php echo stripslashes(htmlspecialchars_decode($video_embed)) ?>
-	</div>
-<?php endif; ?>
