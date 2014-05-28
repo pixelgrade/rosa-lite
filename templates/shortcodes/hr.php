1318a065f5fb20b575d01547f619ec660ef3ddae
Vlad Olaru <vlad.olaru@pixelgrade.com>
2014-05-28 15:33:20 +0300

    separator shortcode

--- templates/shortcodes/hr.php
+++ templates/shortcodes/hr.php
@@ -3,7 +3,9 @@
     // get needed classes
     $classes = 'pixcode  pixcode--separator  separator';
     $classes.= !empty($style) ? ' separator--'.$style : '';
+	$classes.= !empty($color) ? ' separator_color--'.$color : '';
+//	$classes.= !empty($align) ? ' align-'.$align : '';
     // create class attribute
-    $classes = $classes !== '' ? 'class="'.$classes.'"' : '';
+    $classes =  'class="'.trim($classes).'"' ;
 
 echo '<hr '.$classes.'/>';
