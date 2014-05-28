609cded413c6c114ee02a220a37b5e4cacfbcbdf
Vlad Olaru <vlad.olaru@pixelgrade.com>
2014-05-28 22:05:26 +0300

    archive date

--- templates/post/loop-content/header-classic.php
+++ templates/post/loop-content/header-classic.php
@@ -1,19 +1,26 @@
 <?php
 //post format specific
 $post_format = get_post_format();
-if (empty($post_format) || $post_format == 'standard') {
+if ( empty( $post_format ) || $post_format == 'standard' ) {
 	$post_format = '';
+}
+
+$date = get_the_time( get_option( 'date_format' ) );
+
+if ( wpgrade::option( 'blog_custom_date_separator' ) ) {
+	//we need to replace separators with our custom markup
+	$date = str_replace( ', ', ' ', $date );
+	$date = str_replace( '/ ', ' ', $date );
+	$date = str_replace( '  ', ' ', $date );
+
+	$date = str_replace( ' ', '<span class="date__dot"></span>', $date );
 }
 ?>
 
 <header>
-    <div class="article__date">
-        <abbr class="published" title="<?php the_time('c'); ?>"><?php the_time( get_option( 'date_format' ) ); ?></abbr>
-        <span class="date__dot"></span>
-        <abbr class="published" title="<?php the_time('c'); ?>"><?php the_time( get_option( 'date_format' ) ); ?></abbr>
-        <span class="date__dot"></span>
-        <abbr class="published" title="<?php the_time('c'); ?>"><?php the_time( get_option( 'date_format' ) ); ?></abbr>
-    </div>
-    <h2 class="article__title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-    <span class="separator-stylish  separator-stylish--2">✻</span>
+	<div class="article__date">
+		<time class="published" datetime="<?php echo esc_attr( get_the_date( 'c' ) ); ?>"><?php echo $date ?></time>
+	</div>
+	<h2 class="article__title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+	<span class="separator-stylish  separator-stylish--2">&#10043;</span>
 </header>
