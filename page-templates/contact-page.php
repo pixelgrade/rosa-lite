7f1948b7094a85fbd54a9b0b0a12bcdf13136f89
Vlad Olaru <vlad.olaru@pixelgrade.com>
2014-05-29 14:51:50 +0300

    page cover metaboxes

--- page-templates/contact-page.php
+++ page-templates/contact-page.php
@@ -9,35 +9,41 @@
 get_header();
 
 ?>
+<section class="content  content--single-page">
+	<?php while ( have_posts() ) : the_post(); ?>
+		<article id="post-<?php the_ID(); ?>" <?php post_class( "article--page  article--main" ); ?>>
+			<header class="article__header">
+				<?php
+				//get the Google Maps URL
+				$gmap_url = get_post_meta( wpgrade::lang_page_id( get_the_ID() ), wpgrade::prefix() . 'gmap_url', true );
+				if ( ! empty( $gmap_url ) ) :
+					$gmap_custom_style   = get_post_meta( wpgrade::lang_page_id( get_the_ID() ), wpgrade::prefix() . 'gmap_custom_style', true );
+					$gmap_marker_content = get_post_meta( wpgrade::lang_page_id( get_the_ID() ), wpgrade::prefix() . 'gmap_maker_content', true );
 
-	<div id="main" class="content content--contact djax-updatable">
+					?>
+					<div id="gmap"
+					     data-url="<?php esc_attr_e( $gmap_url ); ?>" <?php echo ( $gmap_custom_style == 'on' ) ? 'data-customstyle' : ''; ?>
+					     data-markercontent="<?php echo esc_attr( $gmap_marker_content ); ?>"></div>
+				<?php endif; ?>
+			</header>
+			<section class="article__content">
+				<div class="container">
+					<section class="page__content  js-post-gallery  cf">
+						<h1 class="entry__title"><?php the_title(); ?></h1>
+						<?php the_content(); ?>
+					</section>
+				</div>
+			</section>
+		</article>
+		<?php get_template_part( 'templates/subpages' ); ?>
 		<?php
-		//get the Google Maps URL
-		$gmap_url = get_post_meta( wpgrade::lang_page_id( get_the_ID() ), wpgrade::prefix() . 'gmap_url', true );
-		if ( ! empty( $gmap_url ) ) {
-			$gmap_custom_style   = get_post_meta( wpgrade::lang_page_id( get_the_ID() ), wpgrade::prefix() . 'gmap_custom_style', true );
-			$gmap_marker_content = get_post_meta( wpgrade::lang_page_id( get_the_ID() ), wpgrade::prefix() . 'gmap_maker_content', true );
-
-			?>
-			<div id="gmap" data-url="<?php esc_attr_e( $gmap_url ); ?>" <?php echo ( $gmap_custom_style == 'on' ) ? 'data-customstyle' : ''; ?> data-markercontent="<?php echo esc_attr( $gmap_marker_content ); ?>"></div>
-		<?php } ?>
-		<div class="page-content entry__body">
-			<div class="page-main">
-				<?php while ( have_posts() ) : the_post(); ?>
-					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-						<header class="entry-header">
-							<h1 class="entry__title"><?php the_title(); ?></h1>
-
-							<div class="bleed--left">
-								<hr class="separator separator--dotted grow">
-							</div>
-						</header>
-						<div class="entry__content"><?php the_content(); ?></div>
-					</article>
-				<?php endwhile; ?>
+		//comments
+		if ( comments_open() || '0' != get_comments_number() ): ?>
+			<div class="container">
+				<?php comments_template(); ?>
 			</div>
-		</div>
-		<!-- .page-content -->
-	</div>
+		<?php endif; ?>
+	<?php endwhile; ?>
+</section>
 
 <?php get_footer();
