ea5ab34a74a11afc5a471f561833d7dc530d54fa
Razvan Onofrei <razwan.me@gmail.com>
2014-05-31 19:54:48 +0300

    add transitions, tweak performance, comment the code

--- assets/js/main.js
+++ assets/js/main.js
@@ -594,35 +594,41 @@ function navigatorInit() {
 
     var $navigator      = $('.navigator'),
         $headers        = $('.article__header'),
-        lastSelected    = 0;
+        currentSelected = 0,
+        lastSelected    = 0,
+        isWhite         = true,
+        wasWhite        = true,
+        latestKnownScrollY = window.scrollY,
+        ticking = false;
 
+    // if we're not on a page or there's only one header ABORT MISSION!
     if (!$navigator.length || $headers.length < 2) {
         return;
     }
 
+    // add bullets to the indicator for each header found
     for (var i = 0; i < $headers.length; i = i + 1) {
         $('<div class="navigator__item"></div>').appendTo($navigator);
     }
 
+    // add an indicator for the section that's currently in the viewport
     var $selected = $('<div class="navigator__item  navigator__item--selected"></div>').appendTo($navigator);
 
+    // after all the bullets have been added vertically center the navigator
     $navigator.css({
         'margin-top': -1 * $navigator.height() / 2
     });
 
+    // update
     requestTick();
-
     TweenMax.to($navigator, 0.3, {opacity: 1});
 
-    var latestKnownScrollY = window.scrollY,
-        ticking = false;
-
+    // function used to update navigator's color and indicator's position
     function update() {
 
-        var currentSelected;
-
         ticking = false;
 
+        // loop through each header and find current state
         $headers.each(function(i, header) {
 
             var $header         = $(header),
@@ -632,18 +638,27 @@ function navigatorInit() {
 
             if (navigatorMiddle > headerTop) {
                 currentSelected = i;
-                $navigator.removeClass('navigator--black');
+                isWhite = true;
 
                 if (navigatorMiddle > headerBottom) {
-                    $navigator.addClass('navigator--black');
+                    isWhite = false;
                 }
             }
 
         });
 
+        // if the navigator's indicator has to be moved
+        // then move it accordingly and update state
         if (lastSelected != currentSelected) {
             lastSelected = currentSelected;
-            TweenMax.to($selected, 0.3, {top: 24 * currentSelected, ease: 'Bounce'});
+            TweenMax.to($selected, 0.3, {top: 24 * currentSelected});
+        }
+
+        // if the navigator's color has to be changed
+        // then change it accordingly and update state
+        if (wasWhite != isWhite) {
+            wasWhite = isWhite;
+            $navigator.toggleClass('navigator--black', !isWhite);
         }
 
     }
