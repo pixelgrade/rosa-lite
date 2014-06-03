8589d5c774e40b4dfc046e4639696972389f9574
andreilupu <andrei.lupu@pixelgrade.com>
2014-06-03 10:57:31 +0300

    add back custom fonts

--- templates/core/inline-custom-css.php
+++ templates/core/inline-custom-css.php
@@ -1,56 +1,99 @@
 <?php
+
 /**
  * Generate all the css declared in customizer's config
+ * ====== DO NOT EDIT BELOW!!! =======
+ * If you need to add custom css rules add them above so we can keep track of them
  */
 
+$redux_sections = wpgrade::get_redux_sections();
 
-/* @var string $main_color */
-/* @var array  $fonts */
-/* @var string $rgb */
+if ( is_array( $redux_sections ) || is_object( $redux_sections ) ) {
+	foreach ( $redux_sections as $key => $section ) {
 
-$main_color = wpgrade::option('main_color');
+		if ( isset( $section['fields'] ) ) {
 
+			foreach ( $section['fields'] as $i => $field ) {
 
-$rgb = implode(',', wpgrade::hex2rgb_array($main_color));
+				if ( isset( $field['customizer'] ) && isset( $field['customizer']['css_rules'] ) ) {
+
+					foreach ( $field['customizer']['css_rules'] as $key => $rule ) {
+
+						//rebuild the option value for each rule
+						$option_value = wpgrade::option( $field['id'] );
+
+						// @TODO  make from this a method used also in customizer
+						wpgrade::display_dynamic_css_rule( $rule, $key, $option_value );
+					}
+				}
+			}
+		}
+	}
+}
+
+/**
+ *======= TYPOGRAPHY
+ */
+
 $fonts = array();
 
-if (wpgrade::option('use_google_fonts')) {
-	$fonts_array = array
-	(
+if ( wpgrade::option( 'use_google_fonts' ) ) {
+	$fonts_array = array(
 		'google_titles_font',
 		'google_subtitles_font',
 		'google_nav_font',
 		'google_body_font'
 	);
 
-	foreach ($fonts_array as $font) {
-		$the_font = wpgrade::get_the_typo($font);
-		if ( isset($the_font['font-family'] ) && ! empty($the_font['font-family'])) {
-			$fonts[$font] = $the_font;
+	foreach ( $fonts_array as $font ) {
+		$the_font = wpgrade::get_the_typo( $font );
+		if ( isset( $the_font['font-family'] ) && ! empty( $the_font['font-family'] ) ) {
+			$fonts[ $font ] = $the_font;
 		}
 	}
 }
 
+if ( isset( $fonts['google_titles_font'] ) ) { ?>
+	/* Select classes here */
+	h1, h2, h3, h4, h5, h6, hgroup,
+	h1 a, h2 a, h3 a, h4 a, h5 a, h6 a,
+	blockquote,
+	.tabs__nav, .popular-posts__time,
+	.pagination li a, .pagination li span{
+	<?php wpgrade::display_font_params( $fonts['google_titles_font'] ); ?>
+	}
 
-$redux_sections =  wpgrade::get_redux_sections();
+<?php
+}
 
-foreach ( $redux_sections as $key => $section ){
+if ( isset( $fonts['google_nav_font'] ) ) { ?>
+	/* Select classes here */
+	.navigation a{
+	<?php wpgrade::display_font_params( $fonts['google_nav_font'] ); ?>
+	}
 
-	if ( isset( $section['fields'] ) ) {
+<?php
+}
 
-		foreach ($section['fields'] as $i => $field) {
-
-			if ( isset($field['customizer']) && isset($field['customizer']['css_rules']) ) {
-
-				foreach ( $field['customizer']['css_rules'] as $key => $rule) {
-
-					//rebuild the option value for each rule
-					$option_value = wpgrade::option( $field['id']);
-
-					// @TODO  make from this a method used also in customizer
-					wpgrade::display_dynamic_css_rule( $rule, $key, $option_value );
-				}
-			}
+if ( isset( $fonts['google_body_font'] ) ) {
+	// this needs a default
+	$font_size = '14px'; ?>
+	/* Select classes here */
+	html, .wp-caption-text, .small-link,
+	.post-nav-link__label, .author__social-link,
+	.comment__links, .score__desc  {
+	<?php wpgrade::display_font_params( $fonts['google_body_font'] ); ?>
+	}
+	<?php if ( isset( $fonts['google_body_font']['font-size'] ) ) {
+		$font_size = $fonts['google_body_font']['font-size'];
+		unset( $fonts['google_body_font']['font-size'] ); ?>
+		/* Size Classes */
+		.article, .single .main, .page .main,
+		.comment__content,
+		.footer__widget-area  {
+		font-size: <?php echo $font_size ?>;
 		}
+
+	<?php
 	}
 }
