1a7c5400d42e50bea0a2ce72acad3db24762d6c1
cristianfrumusanu <cristian.frumusanu@gmail.com>
2014-06-05 12:24:26 +0300

    buttons fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1147,7 +1147,10 @@ function eventHandlers() {
 
     if(ieMobile) filterHandler = 'click';
 
-    $('.pix-dropdown').on(filterHandler, function(){
+    $('.pix-dropdown').on(filterHandler, function(e){
+        e.preventDefault();
+        e.stopPropagation();
+
         $(this).toggleClass('active');
     });
 
