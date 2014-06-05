5c2071b7ffed7b24cc8f7110d9b02af651d17532
andreilupu <andrei.lupu@pixelgrade.com>
2014-06-05 15:17:26 +0300

    dunno why but commit again

--- wpgrade-core/vendor/redux3/inc/fields/select_image/field_select_image.js
+++ wpgrade-core/vendor/redux3/inc/fields/select_image/field_select_image.js
@@ -1,16 +1,30 @@
-(function($) {
+(function( $ ) {
     "use strict";
 
-    $('.redux-select-image-item').on('change', function() {
-        var preview = $(this).parents('.redux-field:first').find('.redux-preview-image');
+    $.reduxSelectImage = $.reduxSelectImage || {};
 
-        if ($(this).val() === "") {
-            preview.fadeOut('medium', function() {
-                preview.attr('src', '');
-            });
-        } else {
-            preview.attr('src', $(this).val());
-            preview.fadeIn().css('visibility', 'visible');
+    $( document ).ready(
+        function() {
+            $.reduxSelectImage.init();
         }
-    });
-})(jQuery);
+    );
+
+    $.reduxSelectImage.init = function() {
+        $( '.redux-select-image-item' ).on(
+            'change', function() {
+                var preview = $( this ).parents( '.redux-field:first' ).find( '.redux-preview-image' );
+
+                if ( $( this ).val() === "" ) {
+                    preview.fadeOut(
+                        'medium', function() {
+                            preview.attr( 'src', '' );
+                        }
+                    );
+                } else {
+                    preview.attr( 'src', $( this ).val() );
+                    preview.fadeIn().css( 'visibility', 'visible' );
+                }
+            }
+        );
+    };
+})( jQuery );
--- wpgrade-core/vendor/redux3/inc/fields/sortable/field_sortable.js
+++ wpgrade-core/vendor/redux3/inc/fields/sortable/field_sortable.js
@@ -1,53 +1,70 @@
 /*global jQuery, document, redux_change */
 
-(function($) {
+(function( $ ) {
     "use strict";
-    
-    $(document).ready(function() {
-        
-        var scroll = '';
-        var $scrollable = $(".redux-sorter");
-        function scrolling(){
-            if (scroll == 'up') {
-                $scrollable.scrollTop($scrollable.scrollTop()-20);
-                setTimeout(scrolling,50);
-            } else if (scroll == 'down'){
-                $scrollable.scrollTop($scrollable.scrollTop()+20);
-                setTimeout(scrolling,50);
-            }
-        }          
-        
-        $(".redux-sortable").sortable({
-            handle: ".drag",
-            placeholder:    "placeholder",
-            opacity: 0.7,
-            scroll: false,
-            out: function( event, ui ) {
-                if (!ui.helper) return;
-                if (ui.offset.top>0) {
-                    scroll='down';
-                } else {
-                    scroll='up';
-                }
-                scrolling();
-            },
-            over: function( event, ui ) {
-                scroll='';
-            },
-            deactivate:function( event, ui ) {
-                scroll='';
-            },                            
-            update: function() {
-                redux_change($(this));
-            }
-        });
 
-        $('.checkbox_sortable').on('click', function() {
-            if ($(this).is(":checked")) {
-                $('#' + $(this).attr('rel')).val(1);
-            } else {
-                $('#' + $(this).attr('rel')).val('');
+    $.reduxSortable = $.reduxSortable || {};
+
+    var scroll = '';
+
+    $( document ).ready(
+        function() {
+            $.reduxSortable.init();
+        }
+    );
+
+    $.reduxSortable.scrolling = function() {
+        var $scrollable = $( ".redux-sorter" );
+
+        if ( scroll == 'up' ) {
+            $scrollable.scrollTop( $scrollable.scrollTop() - 20 );
+            setTimeout( scrolling, 50 );
+        } else if ( scroll == 'down' ) {
+            $scrollable.scrollTop( $scrollable.scrollTop() + 20 );
+            setTimeout( scrolling, 50 );
+        }
+    };
+
+    $.reduxSortable.init = function() {
+
+        $( ".redux-sortable" ).sortable(
+            {
+                handle: ".drag",
+                placeholder: "placeholder",
+                opacity: 0.7,
+                scroll: false,
+                out: function( event, ui ) {
+                    if ( !ui.helper ) return;
+                    if ( ui.offset.top > 0 ) {
+                        scroll = 'down';
+                    } else {
+                        scroll = 'up';
+                    }
+                    $.reduxSortable.scrolling();
+                },
+
+                over: function( event, ui ) {
+                    scroll = '';
+                },
+
+                deactivate: function( event, ui ) {
+                    scroll = '';
+                },
+
+                update: function() {
+                    redux_change( $( this ) );
+                }
             }
-        });
-    });
-})(jQuery);
+        );
+
+        $( '.checkbox_sortable' ).on(
+            'click', function() {
+                if ( $( this ).is( ":checked" ) ) {
+                    $( '#' + $( this ).attr( 'rel' ) ).val( 1 );
+                } else {
+                    $( '#' + $( this ).attr( 'rel' ) ).val( '' );
+                }
+            }
+        );
+    };
+})( jQuery );
