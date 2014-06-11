c9fc0887e4391c4b9a8bd327289990a69157c146
cristianfrumusanu <cristian.frumusanu@gmail.com>
2014-06-11 16:59:59 +0300

    products archive 90% #10

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -52,6 +52,28 @@ if ( 0 == $woocommerce_loop['loop'] % $woocommerce_loop['columns'] )
 		?>
 
 		<h4 class="product__title"><?php the_title(); ?></h4>
+
+        <?php
+            // Getting the categories for each product
+            $terms = get_the_terms( $product->id, 'product_cat' );
+            if( !empty($terms) && ! is_wp_error( $terms ) ): ?>
+
+            <ul class="product__cats  nav">
+
+            <?php
+
+            // Getting only the first 3 categories
+            $terms = array_slice($terms, 0, 3);
+
+            foreach ( $terms as $term ) {
+                echo '<li class="product__cat"><span rel="category">' . $term->name . '</span></li>';
+            }
+            ?>
+
+            </ul>
+
+            <?php endif;
+        ?>
         <hr/>
 
 		<?php
