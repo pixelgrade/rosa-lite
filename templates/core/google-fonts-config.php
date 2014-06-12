4a5254cce6a3bacad992bb9eb2e49e41a44dc3d7
andreilupu <andrei.lupu@pixelgrade.com>
2014-06-12 12:21:03 +0300

    fix customizer typography

--- templates/core/google-fonts-config.php
+++ templates/core/google-fonts-config.php
@@ -10,18 +10,21 @@ if ( class_exists('ReduxFramework_customizer_typography')) {
 
 	// on customizer overwrite options
 	// @TODO Refactor this somewhere else
-//	if ( isset( $_POST['customized'] ) ) {
-//		$typography2 = false;
-//		foreach ( $fonts_array  as $font ) {
-//			if ( wpgrade::customizer_option_exists( $font ) ) {
-//				$this_font = wpgrade::get_customizer_option( $font );
-//				$typography2 = new ReduxFramework_customizer_typography( null, $this_font, $redux );
-//			}
-//		}
-//		$test = 'test';
-//	}
+	if ( isset( $_POST['customized'] ) ) {
+		$typography2 = false;
+		foreach ( $fonts_array  as $font ) {
+			if ( wpgrade::customizer_option_exists( $font ) ) {
+				$this_font = wpgrade::get_customizer_option( $font );
 
-
+				// Regenerate the font-list on customizer page
+				$typography2 = new ReduxFramework_customizer_typography( array('google' => 1), $this_font, $redux );
+				ob_start();
+				$typography2->output();
+				$to_be_deleted = ob_get_clean();
+				unset($to_be_deleted); // we actually don't need this variable
+			}
+		}
+	}
 	$families = $typography->makeGoogleWebfontString( $typography->parent->typography ); ?>
 	<script type="text/javascript">
 		WebFont.load({ google: {families: [<?php echo ($families); ?>]} });
