1f669ee0237f6825d8e4172b8ead24e776c5f471
Razvan Onofrei <razwan.me@gmail.com>
2014-06-23 16:44:19 +0300

    fuck you very much IE

--- header.php
+++ header.php
@@ -12,12 +12,12 @@
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=<?php bloginfo( 'charset' ); ?>">
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="HandheldFriendly" content="True">
-	<meta name="apple-touch-fullscreen" content="yes"/>
-	<meta name="MobileOptimized" content="320">
-	<title><?php wp_title( '|', true, 'right' ); ?></title>
+    <meta name="HandheldFriendly" content="True">
+    <meta name="apple-touch-fullscreen" content="yes"/>
+    <meta name="MobileOptimized" content="320">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php
