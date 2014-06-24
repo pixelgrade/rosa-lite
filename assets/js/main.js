9a9722680109dcce66246c3b01971e0d5d172359
cristianfrumusanu <cristian.frumusanu@gmail.com>
2014-06-24 10:49:18 +0300

    fixed header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1099,11 +1099,11 @@ function stickyHeaderInit() {
         offset: offset - headerHeight - 1,
         // animate with GSAP
         onPin: function () {
-            TweenMax.to($header, 0.1, {top: ''});
+//            TweenMax.to($header, 0.1, {top: ''});
         },
         onUnpin: function () {
             if ($('html').hasClass('navigation--is-visible')) {return}
-            TweenMax.to($header, 0.1, {top: -1 * headerHeight});
+//            TweenMax.to($header, 0.1, {top: -1 * headerHeight});
         }
     });
 }
