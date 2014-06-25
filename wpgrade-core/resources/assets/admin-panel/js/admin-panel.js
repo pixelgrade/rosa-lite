4eba1506a037de18949545c59ac11c511eed6598
cristianfrumusanu <cristian.frumusanu@gmail.com>
2014-06-25 16:15:29 +0300

    changes

--- wpgrade-core/resources/assets/admin-panel/js/admin-panel.js
+++ wpgrade-core/resources/assets/admin-panel/js/admin-panel.js
@@ -477,12 +477,5 @@
 		fixDiv();
 	});
 
-    $('.redux-group-menu a').on('click', function() {
-        setTimeout(function(){
-            var text = $('.redux-group-tab:visible > h3').html();
-
-            $('#floating-title').html(text);
-        }, 100);
-    });
 
 })(jQuery, window);
