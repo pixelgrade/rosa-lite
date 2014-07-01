690f396f4c941da88b58fc02d938cff4706f3095
Razvan Onofrei <razwan.me@gmail.com>
2014-07-01 18:27:23 +0300

    whaaaa

--- assets/js/main/main.js
+++ assets/js/main/main.js
@@ -5,46 +5,45 @@
 function niceScrollInit() {
     if (globalDebug) {console.log("NiceScroll Init");}
 
-    var smoothScroll = $('body').data('smoothscrolling') !== undefined;
-
-    if (smoothScroll && !is_OSX && !Modernizr.touch && !is_mobile_ie) {
-
-        var $window = $(window);		// Window object
-
-        $window.on("mousewheel DOMMouseScroll", function(event) {
-
-            var scrollTo,
-                scrollDistance  = 400,
-                delta;
-
-            if (event.type == 'mousewheel') {
-                delta    = event.originalEvent.wheelDelta / 120;
-            }
-            else if (event.type == 'DOMMouseScroll') {
-                delta    = - event.originalEvent.detail / 3;
-            }
-
-            scrollTo = latestKnownScrollY - delta * scrollDistance;
-
-            if (scrollTo) {
-
-                event.preventDefault();
-
-                TweenMax.to($window, .6, {
-                    scrollTo: {
-                        y:          scrollTo,
-                        autoKill:   true
-                    },
-                    ease:           Power1.easeOut,	// For more easing functions see http://api.greensock.com/js/com/greensock/easing/package-detail.html
-                    autoKill:       true,
-                    overwrite:      5
-                });
-
-            }
-
-        });
-
-    }
+//    var smoothScroll = $('body').data('smoothscrolling') !== undefined;
+//
+//    if (smoothScroll && !is_OSX && !Modernizr.touch && !is_mobile_ie) {
+//        var $window = $(window);		// Window object
+//
+//        $window.on("mousewheel DOMMouseScroll", function(event) {
+//
+//            var scrollTo,
+//                scrollDistance  = 400,
+//                delta;
+//
+//            if (event.type == 'mousewheel') {
+//                delta    = event.originalEvent.wheelDelta / 120;
+//            }
+//            else if (event.type == 'DOMMouseScroll') {
+//                delta    = - event.originalEvent.detail / 3;
+//            }
+//
+//            scrollTo = latestKnownScrollY - delta * scrollDistance;
+//
+//            if (scrollTo) {
+//
+//                event.preventDefault();
+//
+//                TweenMax.to($window, .6, {
+//                    scrollTo: {
+//                        y:          scrollTo,
+//                        autoKill:   true
+//                    },
+//                    ease:           Power1.easeOut,	// For more easing functions see http://api.greensock.com/js/com/greensock/easing/package-detail.html
+//                    autoKill:       true,
+//                    overwrite:      5
+//                });
+//
+//            }
+//
+//        });
+//
+//    }
 
 }
 
