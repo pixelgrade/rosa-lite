cf47a575b2dfe6d94fa11839f0a7db4973aaf23f
andreilupu <andrei.lupu@pixelgrade.com>
2017-06-23 16:27:43 +0300

    add back pixcare install

--- inc/integrations/pixcare.php
+++ inc/integrations/pixcare.php
@@ -12,12 +12,8 @@
  */
 function rosa_force_redirect_to_pixcare_install_once() {
 	$plugin_version = get_option( 'pixelgrade_care_version' );
-//	$plugin_files   = get_plugin_files( 'pixelgrade-care/pixelgrade-care.php' );
-//	if ( ! function_exists( 'get_plugin_files' ) ) {
-//		require_once ABSPATH . 'wp-admin/includes/plugin.php';
-//	}
 
-	if ( class_exists( 'PixelgradeCare' ) ) {
+	if ( class_exists( 'PixelgradeCare' ) || ! file_exists( WP_PLUGIN_DIR . '/pixelgrade-care/pixelgrade-care.php' ) ) {
 		return;
 	}
 
