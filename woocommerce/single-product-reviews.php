45f7264d23403cf70674a16e298b182f79497b49
Vlad Olaru <vlad.olaru@pixelgrade.com>
2017-12-07 10:31:39 +0200

    updated woocommerce templates - fixes #402

--- woocommerce/single-product-reviews.php
+++ woocommerce/single-product-reviews.php
@@ -13,8 +13,9 @@
  * @see 	    https://docs.woocommerce.com/document/template-structure/
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     2.3.2
+ * @version     3.2.0
  */
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
@@ -26,14 +27,15 @@ if ( ! comments_open() ) {
 }
 
 ?>
-<div id="reviews">
+<div id="reviews" class="woocommerce-Reviews">
 	<div id="comments">
-		<h2><?php
-			if ( get_option( 'woocommerce_enable_review_rating' ) === 'yes' && ( $count = $product->get_review_count() ) )
-				printf( _n( '%s review for %s', '%s reviews for %s', $count, 'woocommerce' ), $count, get_the_title() );
-			else
+		<h2 class="woocommerce-Reviews-title"><?php
+			if ( get_option( 'woocommerce_enable_review_rating' ) === 'yes' && ( $count = $product->get_review_count() ) ) {
+				/* translators: 1: reviews count 2: product name */
+				printf( esc_html( _n( '%1$s review for %2$s', '%1$s reviews for %2$s', $count, 'woocommerce' ) ), esc_html( $count ), '<span>' . get_the_title() . '</span>' );
+			} else {
 				_e( 'Reviews', 'woocommerce' );
-			?></h2>
+			} ?></h2>
 
 		<?php if ( have_comments() ) : ?>
 
@@ -66,7 +68,7 @@ if ( ! comments_open() ) {
 				$commenter = wp_get_current_commenter();
 
 				$comment_form = array(
-					'title_reply'          => have_comments() ? __( 'Add a review', 'woocommerce' ) : __( 'Be the first to review', 'woocommerce' ) . ' &ldquo;' . get_the_title() . '&rdquo;',
+					'title_reply'          => have_comments() ? __( 'Add a review', 'woocommerce' ) : sprintf( __( 'Be the first to review &ldquo;%s&rdquo;', 'woocommerce' ), get_the_title() ),
 					'title_reply_to'       => __( 'Leave a Reply to %s', 'woocommerce' ),
 					'comment_notes_before' => '',
 					'comment_notes_after'  => '',
@@ -82,7 +84,7 @@ if ( ! comments_open() ) {
 					);
 
 				if ( get_option( 'woocommerce_enable_review_rating' ) === 'yes' ) {
-					$comment_form['comment_field'] = '<p class="comment-form-rating"><label for="rating">' . __( 'Your rating', 'woocommerce' ) .'</label><select name="rating" id="rating">
+					$comment_form['comment_field'] = '<p class="comment-form-rating"><label for="rating">' . __( 'Your rating', 'woocommerce' ) .'</label><select name="rating" id="rating" aria-required="true" required>
 							<option value="">' . esc_html__( 'Rate&hellip;', 'woocommerce' ) . '</option>
 							<option value="5">' . esc_html__( 'Perfect', 'woocommerce' ) . '</option>
 							<option value="4">' . esc_html__( 'Good', 'woocommerce' ) . '</option>
