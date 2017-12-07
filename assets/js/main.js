84da50068c72d7428f915a2e7cfde8a1e9f66012
Razvan <razwan.me@gmail.com>
2017-12-07 16:28:06 +0200

    Add mutation observer to trigger rellax initialization on content change - closes #397

--- assets/js/main.js
+++ assets/js/main.js
@@ -1248,6 +1248,35 @@ GMap.prototype.customStyle = [
 	}
 ];
 
+(function($) {
+
+	function observe( $container ) {
+		var MutationObserver = window.MutationObserver,
+			observer, config;
+
+		if ( typeof MutationObserver === "undefined" ) {
+			return;
+		}
+
+		observer = new MutationObserver( function() {
+			$( window ).trigger( 'rellax' );
+		} );
+
+		config = {
+			childList: true,
+			characterData: false,
+			attributes: false,
+			subtree: true
+		};
+
+		$container.each( function() {
+			observer.observe( this, config );
+		} );
+	}
+
+	observe( $('.article__content') );
+
+})(jQuery);
 /* --- Royal Slider Init --- */
 
 function royalSliderInit( $container ) {
