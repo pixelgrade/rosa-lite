1c113ca4c61f21347ef69f241de5e6b60505d06f
Cosmin Burloiu <cosmin@Cosmins-MacBook-Pro.local>
2018-10-23 14:43:38 +0300

    solved issue 439

--- assets/js/admin/customizer_preview.js
+++ assets/js/admin/customizer_preview.js
@@ -6,11 +6,13 @@
 ( function( $, window ) {
 
 	// Site title and description.
-	wp.customize( 'blogname', function( value ) {
-		value.bind( function( to ) {
-			$( '.site-title a' ).text( to );
-		} );
-	} );
+    if (!document.getElementsByClassName('site-logo--image')) {
+        wp.customize('blogname', function (value) {
+            value.bind(function (to) {
+                $('.site-title a').text(to);
+            });
+        });
+    }
 	wp.customize( 'blogdescription', function( value ) {
 		value.bind( function( to ) {
 			$( '.site-description' ).text( to );
