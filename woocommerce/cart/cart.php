f9ed5f1c4cd3e5f8194c6de6b8f048af5d429bb6
Burloiu Cosmin <cosmin@pixelgrade.com>
2019-02-20 17:50:56 +0200

    escaped string

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -115,7 +115,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 							echo apply_filters( 'woocommerce_cart_item_remove_link', sprintf(
 								'<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">&times;</a>',
 								esc_url( wc_get_cart_remove_url( $cart_item_key ) ),
-								__( 'Remove this item', 'woocommerce' ),
+								esc_html__( 'Remove this item', 'woocommerce' ),
 								esc_attr( $product_id ),
 								esc_attr( $_product->get_sku() )
 							), $cart_item_key );
