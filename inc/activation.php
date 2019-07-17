8338a2967e8007c2c65e4c0627bb307f308b16d4
Cosmin Burloiu <cosmin@pixelgrade.com>
2019-07-17 14:47:05 +0300

    modified metaboxes

--- inc/activation.php
+++ inc/activation.php
@@ -46,12 +46,48 @@ return array(
 								'name'  => __( 'Simple', 'rosa-lite' ),
 								'value' => 'simple',
 							),
-							array(
-								'name'  => __( 'Waves', 'rosa-lite' ),
-								'value' => 'waves',
-							),
 						),
 						'std'     => 'simple',
+					),
+				),
+			),
+			//options for the Page Header Covers
+			'rosa_page_header_area_cover'       => array(
+				'id'         => 'rosa_page_header_area_cover',
+				'title'      => __( 'Featured Header Area', 'rosa-lite' ),
+				'pages'      => array( 'page' ), // Post type
+				'context'    => 'normal',
+				'priority'   => 'high',
+				'hidden'     => true,
+				'show_on'    => array(
+					'key' => 'page-template',
+					'value' => array( 'default', 'page-templates/slideshow.php' ),
+				),
+				'show_names' => true, // Show field names on the left
+				'fields'     => array(
+					array(
+						'name' => __( 'Header Height', 'rosa-lite' ),
+						'desc' => '<p class="cmb_metabox_description">' . __( 'Select the height of the header area in relation to the browser window.', 'rosa-lite' ).'</p>',
+						'id'   => wpgrade::prefix() . 'page_header_height',
+						'type'    => 'select',
+						'options' => array(
+							array(
+								'name'  => __( '&#9673;&#9673;&#9673; Full Height', 'rosa-lite' ),
+								'value' => 'full-height',
+							)
+						),
+						'std'     => 'full-height',
+					),
+					array(
+						'name' => __( 'Subtitle', 'rosa-lite' ),
+						'id'   => wpgrade::prefix() . 'page_cover_subtitle',
+						'type' => 'text',
+					),
+					array(
+						'name' => __( 'Title', 'rosa-lite' ),
+						'desc' => __( "If left empty we will use the page title. Tip: put a space if you don't want any cover text.", 'rosa-lite' ),
+						'id'   => wpgrade::prefix() . 'page_cover_title',
+						'type' => 'text',
 					),
 				),
 			),
