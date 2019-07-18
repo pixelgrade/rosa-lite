c15fcc4f8c4228eea4f36882f8aba9ddc3f24ec8
Cosmin Burloiu <cosmin@pixelgrade.com>
2019-07-18 17:09:42 +0300

    small typo change

--- inc/activation.php
+++ inc/activation.php
@@ -10,7 +10,7 @@ return array(
 	'pixlikes-settings' => array(
 		'show_on_post'         => false,
 		'show_on_page'         => false,
-		'show_on_hompage'      => false,
+		'show_on_homepage'      => false,
 		'show_on_archive'      => false,
 		'like_action'          => 'click',
 		'hover_time'           => 1000,
@@ -22,16 +22,16 @@ return array(
 			//General page settings
 			'rosa_page_general'       => array(
 				'id'         => 'rosa_page_general',
-				'title'      => __( 'General', 'rosa-lite' ),
+				'title'      => esc_html__( 'General', 'rosa-lite' ),
 				'pages'      => array( 'page' ), // Post type
 				'context'    => 'normal',
 				'priority'   => 'high',
-				'hidden'     => true,
+				'hidden'     => false,
 				'show_names' => true, // Show field names on the left
 				'fields'     => array(
 					array(
-						'name' => __( 'Make Menu Bar Transparent', 'rosa-lite' ),
-						'desc' => __( "This will remove the background from the menu and logo top bar.", 'rosa-lite' ),
+						'name' => esc_html__( 'Make Menu Bar Transparent', 'rosa-lite' ),
+						'desc' => esc_html__( "This will remove the background from the menu and logo top bar.", 'rosa-lite' ),
 						'id'   => wpgrade::prefix() . 'header_transparent_menu_bar',
 						'type' => 'checkbox',
 					),
@@ -40,7 +40,7 @@ return array(
 			//options for the Page Header Covers
 			'rosa_page_header_area_cover'       => array(
 				'id'         => 'rosa_page_header_area_cover',
-				'title'      => __( 'Featured Header Area', 'rosa-lite' ),
+				'title'      => esc_html__( 'Featured Header Area', 'rosa-lite' ),
 				'pages'      => array( 'page' ), // Post type
 				'context'    => 'normal',
 				'priority'   => 'high',
@@ -52,13 +52,13 @@ return array(
 				'show_names' => true, // Show field names on the left
 				'fields'     => array(
 					array(
-						'name' => __( 'Subtitle', 'rosa-lite' ),
+						'name' => esc_html__( 'Subtitle', 'rosa-lite' ),
 						'id'   => wpgrade::prefix() . 'page_cover_subtitle',
 						'type' => 'text',
 					),
 					array(
-						'name' => __( 'Title', 'rosa-lite' ),
-						'desc' => __( "If left empty we will use the page title. Tip: put a space if you don't want any cover text.", 'rosa-lite' ),
+						'name' => esc_html__( 'Title', 'rosa-lite' ),
+						'desc' => esc_html__( "If left empty we will use the page title. Tip: put a space if you don't want any cover text.", 'rosa-lite' ),
 						'id'   => wpgrade::prefix() . 'page_cover_title',
 						'type' => 'text',
 					),
