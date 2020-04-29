b56503fed29480929756cd07047cf1dff5ce1997
Vlad Olaru <vlad.olaru@pixelgrade.com>
2020-04-29 11:58:21 +0300

    Update starter content filter - see pixelgrade/pixelgrade-care#374

--- inc/integrations/pixcare_starter_content.php
+++ inc/integrations/pixcare_starter_content.php
@@ -8,22 +8,21 @@
  * Filter the import action while getting the `rosa_options` option
  *
  * @param $option
+ * @param $demo_key
  *
  * @return mixed
  */
-function rosa_lite_filter_post_option_rosa_options( $option ) {
-	// get the imported data
-	$pixcare_options = get_option( 'pixcare_options' );
+function rosa_lite_filter_post_option_rosa_options( $option, $demo_key ) {
 	// this holds the ids of posts, pages, medias and everything was already imported
-	$imported_options = $pixcare_options['imported_starter_content'];
+	$starter_content = PixelgradeAssistant_Admin::get_option( 'imported_starter_content' );
 
 	// We need to replace the both logos from the demo with the imported attachment id
-	if ( isset( $imported_options['media']['ignored'][ $option['main_logo_light'] ] ) ) {
-		$option['main_logo_light'] = $imported_options['media']['ignored'][ $option['main_logo_light'] ];
+	if ( isset( $option['main_logo_light'] ) && isset( $starter_content[ $demo_key ]['media']['ignored'][ $option['main_logo_light'] ] ) ) {
+		$option['main_logo_light'] = $starter_content[ $demo_key ]['media']['ignored'][ $option['main_logo_light'] ];
 	}
 
-	if ( isset( $imported_options['media']['ignored'][ $option['main_logo_dark'] ] ) ) {
-		$option['main_logo_dark'] = $imported_options['media']['ignored'][ $option['main_logo_dark'] ];
+	if ( isset( $option['main_logo_dark'] ) && isset( $starter_content[ $demo_key ]['media']['ignored'][ $option['main_logo_dark'] ] ) ) {
+		$option['main_logo_dark'] = $starter_content[ $demo_key ]['media']['ignored'][ $option['main_logo_dark'] ];
 	}
 
 	// on demo this is an option, oldy stuff
@@ -32,4 +31,4 @@ function rosa_lite_filter_post_option_rosa_options( $option ) {
 
 	return $option;
 }
-add_filter( 'pixassist_sce_import_post_option_rosa_options', 'rosa_lite_filter_post_option_rosa_options' );
+add_filter( 'pixassist_sce_import_post_option_rosa_options', 'rosa_lite_filter_post_option_rosa_options', 10, 2 );
